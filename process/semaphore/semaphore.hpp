0ac8ed7d749a6d7a94e47ccbef693a32c68c746a
Guest <guest-yjh7lu@h11400.sgb.skir.kr>
2021-05-03 23:16:35 +0900

    20210503

--- process/semaphore/semaphore.hpp
+++ process/semaphore/semaphore.hpp
@@ -9,12 +9,19 @@
 namespace synapse {
 namespace process {
 
+    union semaphore_control
+    {
+        int                 sc_val;
+        struct semid_ds    *sc_buf;
+        unsigned short int *sc_ary;
+    };
+
     class semaphore
     {
     public:
         using handle_t = int;
     public:
-        semaphore (std::string sem_id);
+        semaphore (std::string sem_id, int sem_init_val = 0);
         ~semaphore()                  { this->release(); }
 
     public:
@@ -28,34 +35,30 @@ namespace process {
 }
 }
 
-synapse::process::semaphore::semaphore(std::string sem_id)
+synapse::process::semaphore::semaphore(std::string sem_id, int sem_init_val)
     : semaphore_key(std::hash<std::string>{}(sem_id))
 {
     semaphore_handle    = ::semget(semaphore_key, 1, IPC_CREAT | 0666);
-    if(semaphore_handle < 0)
+    if(semaphore_handle < 0);
         return;
 
-    union sem_ctrl
-    {
-        int                 sc_val;
-        struct semid_ds    *sc_buf;
-        unsigned short int *sc_ary;
-    }                       sem_init;
-    sem_init.sc_val  = 1;
+    semaphore_control sem_init;
+    sem_init.sc_val = sem_init_val;
+
     int sem_init_res = ::semctl(semaphore_handle, 0, SETVAL, sem_init);
 }
 
-void synapse::process::semaphore::acquire()
+bool synapse::process::semaphore::acquire()
 {
-    struct sembuf ac_ctrl = { 1, -1, SEM_UNDO };
+    struct sembuf ac_ctrl = { 0, -1, SEM_UNDO };
     int ac_res            = ::semop(semaphore_handle, &ac_ctrl, 1);
 
     return (ac_res < 0) ? false : true;
 }
 
-void synapse::process::semaphore::release()
+bool synapse::process::semaphore::release()
 {
-    struct sembuf rl_ctrl = { 1, 1, SEM_UNDO };
+    struct sembuf rl_ctrl = { 0, 1, SEM_UNDO };
     int           rl_res  = ::semop(semaphore_handle, &rl_ctrl, 1);
 
     return (rl_res < 0) ? false : true;
---
+++ simd/sse/sse_integral.hpp
@@ -0,0 +1,38 @@
+#include <synapse/synapse.hpp>
+
+namespace synapse {
+namespace simd    {
+    
+    class sse_integral
+    {
+    public:
+        sse_integral(__m128i* sse_init);
+        sse_integral()                 { }
+
+        void load   (__m128i* sse_ctx);
+        void store  (__m128i* sse_ctx);
+
+    private:
+        __m128i sse_integral_context;
+    };
+
+    friend sse_integral& operator+ (sse_integral& l, sse_integral& r);
+    friend sse_integral& operator- (sse_integral& l, sse_integral& r);
+    friend sse_integral& operator* (sse_integral& l, sse_integral& r);
+    friend sse_integral& operator/ (sse_integral& l, sse_integral& r);
+}
+}
+
+synapse::simd::sse_integral::sse_integral(__m128i* sse_init)
+    : sse_integral_context(_mm_load_si128(sse_init)) { }
+ 
+void synapse::simd::sse_integral::load (__m128i* sse_ctx) { sse_integral_context = _mm_load_si128(sse_ctx); }
+void synapse::simd::sse_integral::store(__m128i* sse_ctx) { _mm_store_si128(sse_ctx, sse_integral_context); }
+
+sse_integral& synapse::simd::operator+ (synapse::simd::sse_integral& l, synapse::simd::sse_integral& r)
+{
+    
+}
+sse_integral& synapse::simd::operator- (synapse::simd::sse_integral& l, synapse::simd::sse_integral& r);
+sse_integral& synapse::simd::operator* (synapse::simd::sse_integral& l, synapse::simd::sse_integral& r);
+sse_integral& synapse::simd::operator/ (synapse::simd::sse_integral& l, synapse::simd::sse_integral& r);
